9287971193ac511dd7e0f6e7ddaa7f96cca77749
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-01-10 19:07:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -97,6 +97,8 @@ def apply_interest_if_due(conn, user_id):
     now = datetime.datetime.now()
 
     seconds_passed = (now - last_applied_time).total_seconds() / 60
+    if seconds_passed < 1:
+        return
 
     hours_fraction = seconds_passed / 60
     hourly_interest_rate = c.execute("SELECT interest_rate FROM savings WHERE user_id = ?", (user_id,)).fetchone()[0]
