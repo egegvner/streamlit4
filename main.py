2e21fabf899d41937bcc0c2ee18e18e61e3c8d7a
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-01-13 17:46:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1073,7 +1073,7 @@ def main_account_view(conn, user_id):
     current_balance = c.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,)).fetchone()[0]
 
     st.header("Main Account (Vault)", divider = "rainbow")
-    st.write(f"Current Balance   |   :green[${current_balance:.2f}]")
+    st.write(f"Current Balance   |   :green[${format_balance(current_balance)}]")
 
     col1, col2 = st.columns(2)
     if col1.button("Top Up", type = "primary", use_container_width = True):
