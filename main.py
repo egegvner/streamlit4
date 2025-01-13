89be7487e9d9bd0b2622e3486674715409b375d1
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-01-13 15:22:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def apply_interest_if_due(conn, user_id):
 
     now = datetime.datetime.now()
 
-    seconds_passed = (now - last_applied_time).total_seconds()
+    seconds_passed = (now - last_applied_time).total_seconds() / 60
     if seconds_passed < 1:
         return
 
