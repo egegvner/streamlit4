8a97d691ba8b39da9700af0533b35e02ae2a9e34
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-01-20 17:54:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2071,7 +2071,7 @@ def stocks_view(conn, user_id):
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     df.set_index('Timestamp', inplace=True)
     df_resampled = df.resample(f"{st.session_state.ti}T").mean()  
-    st.line_chart(df_resampled, color = st.session_state.graph_color2)
+    st.line_chart(df_resampled, color = st.session_state.graph_color)
     c1, c2 = st.columns(2)
     sample_rate = c1.slider("Sample Rate (Lower = More Lag)", min_value = 1, max_value = 100, step = 1)
     if c1.button("Set Resampling", use_container_width=True):
