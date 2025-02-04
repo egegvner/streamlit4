ebcbcfd7f88bbc29def4b1b331e3779b08f63e1b
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-04 22:10:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,11 +35,7 @@ st.set_page_config(
 )
 
 hide = """
-        <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            #GithubIcon {visibility: hidden;}
-        </style>
+        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
         """
 st.markdown(hide, unsafe_allow_html=True)
 
