e517e365ed1dc2c065042c08c97e02be5bd735da
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-10 20:35:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4789,5 +4789,5 @@ def add_column_if_not_exists(conn, table_name, column_name, column_type):
 if __name__ == "__main__":
     conn = get_db_connection()
     init_db(conn)
-    conn.cursor().execute("ALTER TABLE country_lands ADD COLUMN available_shares INTEGER DEFAULT 100.0;")
+    conn.cursor().execute("ALTER TABLE country_lands ADD COLUMN available_shares REAL DEFAULT 100.0;")
     main(conn)
