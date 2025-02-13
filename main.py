5078bd363dab6e0551b34643c6a8dc758df3bfc1
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-13 19:10:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4234,6 +4234,6 @@ def add_column_if_not_exists(conn, table_name, column_name, column_type):
 
 if __name__ == "__main__":
     conn = get_db_connection()
-    conn.cursor().execute("ALTER TABLE user_properties ADD COLUMN las_collected DATETIME DEFAULT CURRENT_TIMESTAMP;")
+    conn.cursor().execute("ALTER TABLE users ADD COLUMN loan REAL DEFAULT 0.0;")
     init_db(conn)
     main(conn)
