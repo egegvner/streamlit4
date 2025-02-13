0ff74bce8e111fec558a044fd4f911de5c7d7cb0
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-13 16:09:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4236,7 +4236,7 @@ def add_column_if_not_exists(conn, table_name, column_name, column_type):
 
 if __name__ == "__main__":
     conn = get_db_connection()
-    conn.cursor().execute("ALTER TABLE marketplace_items ADD COLUMN image_url DEFAULT NULL;")
+    conn.cursor().execute("ALTER TABLE stocks ADD COLUMN change_rate REAL DEFAULT 0.1;")
 
     init_db(conn)
     main(conn)
