945c92dea11e4576ec070fe4c8b4dbc83770e591
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-16 18:37:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def write_stream(s, delay = 0, random_delay = False):
 
 @st.cache_resource
 def get_db_connection():
-    return sqlite3.connect("genova.db", check_same_thread = False)
+    return sqlite3.connect("bank-genova.db", check_same_thread = False)
 
 item_colors = {
         "Common":":gray",
