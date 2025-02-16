9dd7d02dd9870078bb8892c494eb5b44ab760b71
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-16 09:46:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ ph = argon2.PasswordHasher(
     parallelism=4       # Number of parallel threads (default: 1)
 )    
 
+if "Dashboard" not in st.session_state:
+    st.session_state.current_menu = "Dashboard"
+
 previous_layout = st.session_state.get("previous_layout", "centered")
 current_layout = "wide" if st.session_state.current_menu == "Blackmarket" or st.session_state.current_menu == "Investments" or st.session_state.current_menu == "Dashboard" or st.session_state.current_menu == "Stocks" or st.session_state.current_menu == "Transaction History" or st.session_state.current_menu == "Inventory" or st.session_state.current_menu == "Marketplace" or st.session_state.current_menu == "Real Estate" or st.session_state.current_menu == "Chat" else "centered"
 
