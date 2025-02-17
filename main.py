08a108ad4cb65e64ef45cc3b7132e4230408f80a
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-17 17:40:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4912,4 +4912,4 @@ if __name__ == "__main__":
     init_db(conn)
     # main(conn)
     st.info("As so many things has gone wrong, we need to take some time to fix our game. Thank you for your co-operation")
-    st.image("https://res.cloudinary.com/triplet/image/upload/v1739785062/Bank_Genova_Inc_qrsltt.png")
+    st.image("https://res.cloudinary.com/triplet/image/upload/v1739785192/Bank_Genova_Inc_f4oofr.png")
