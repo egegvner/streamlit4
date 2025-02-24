3fdf933c5c3057c074d3409e92f151dc1ca1cdcb
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-24 23:31:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -734,7 +734,7 @@ def init_db(conn):
                   loan_start_date,
                   credit_score INTEGER DEFAULT 600,
                   vip_tier TEXT DEFAULT NULL,
-                  card_url TEXT DEFAULT NULL
+                  card_url TEXT DEFAULT 'https://res.cloudinary.com/triplet/image/upload/v1739785192/Bank_Genova_Inc_f4oofr.png'
                   );''')
 
     c.execute('''CREATE TABLE IF NOT EXISTS transactions (
