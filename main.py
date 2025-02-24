8baccce9edeb850ddb4d634eb57002e013acb75a
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-24 23:30:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2476,7 +2476,7 @@ def dashboard(conn, user_id):
     st.text("")
     st.text("")
     vip_tier = c.execute("SELECT vip_tier FROM users WHERE user_id = ?", (user_id,)).fetchone()
-    if vip_tier is not None or vip_tier != "" or vip_tier != "None" or vip_tier != "NULL:
+    if vip_tier is not None or vip_tier != "" or vip_tier != "None" or vip_tier != "NULL":
         vip_tier = vip_tier[0]
         card_url = c.execute("SELECT card_url FROM users WHERE user_id = ?", (user_id,)).fetchone()[0]
         with st.container(border=True):
