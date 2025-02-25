35a4d7d4ce412c7002479e5c257106692a557454
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-25 19:01:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5360,6 +5360,8 @@ def main(conn):
                             if len(new_password) >= 8:
                                 if new_password == confirm_password:
                                     if new_username not in existing_users and new_username != "egegvner" and new_username != "Egegvner" and new_username != "Genova":
+                                        cookieManager.set("user_id", user[0])
+                                        cookieManager.set("username", username)
                                         register_user(conn, new_username, new_password)
                                     else:
                                         st.error("Username already taken.")
