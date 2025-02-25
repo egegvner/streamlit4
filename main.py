192df33d54017f952ab3b5e00f5695e1240df0e8
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-26 07:34:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5353,7 +5353,7 @@ def main(conn):
                         if new_password != "":
                             if len(new_password) >= 8:
                                 if new_password == confirm_password:
-                                    if new_username not in existing_users and new_username != "egegvner" and new_username != "Egegvner" and new_username != "Genova":
+                                    if new_username not in existing_users:
                                         register_user(conn, new_username, new_password)
                                     else:
                                         st.error("Username already taken.")
