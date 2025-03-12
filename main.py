9f8ea8ca3f475f73eefe41ca91f1166f88cec2d7
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-03-12 13:17:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2761,7 +2761,7 @@ def adjust_stock_prices(conn, stock_id, quantity, action):
     
     price, stock_amount = c.execute("SELECT price, stock_amount FROM stocks WHERE stock_id = ?", (stock_id,)).fetchone()
     
-    elasticity_factor = 10
+    elasticity_factor = 1
     
     if action == "buy":
         price_change = (quantity / stock_amount) * elasticity_factor * price
