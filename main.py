e7fba734ca8f9b20e7dee503943e5cd94be98839
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-10-24 08:36:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2065,6 +2065,10 @@ def inventory_view(conn, user_id):
                         cqw2.write(f":gray[Level] :orange[{level}]")
                         cqw2.write(f":gray[Ready In] :green[{int(hours)}] :gray[Hours,] :green[{int(minutes)}] :gray[Minutes]")
 
+                        st.text("")
+                        st.text("")
+                        st.text("")
+                        st.text("")
                         if time_left.total_seconds() < 0:
                             st.success(f"[Accumulated Rent] :green[${format_number(rent_income)}]")
                         else:
